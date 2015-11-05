d34e37244c87a90be5f17e0875e84d0b380594e9
andrii333 <andrii.333@mail.ru>
2015-11-05 20:39:09 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
+apt-get update
+
 apt-get install git
 apt-get install python-pip
 
 pip install flask
 
 git clone 'https://github.com/andrii333/frame_server'
+cd frame_server
+
+git clone 'https://github.com/andrii333/flask_user'
