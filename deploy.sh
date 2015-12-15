2cf867726a20117510ea9aa52fa83dbaa0f897e0
andrii333 <andrii.333@mail.ru>
2015-12-15 15:36:28 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,9 @@ git clone 'https://github.com/andrii333/frame_server'
 cd frame_server
 
 git clone 'https://github.com/andrii333/flask_user'
+
+#install mongodb for users
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
